63695c6c11fe51a6a6f015c5900b74ed051d13f0
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-09 16:35:13 -0300

    reeption dimabe

--- dimabe_reception/__manifest__.py
+++ dimabe_reception/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # noinspection PyStatementEffect
 {
-    'name': "Reception",
+    'name': "Reception Dimabe",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
