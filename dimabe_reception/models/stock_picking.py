a32c6efcb6ff22ca2d9383016aa605a24e714c81
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-09 18:02:49 -0300

    fix modelos en dimabe reception

--- dimabe_reception/models/stock_picking.py
+++ dimabe_reception/models/stock_picking.py
@@ -200,7 +200,7 @@ class StockPicking(models.Model):
 
     @api.multi
     def notify_alerts(self):
-        alert_config = self.env['dimabe_reception.alert.config'].search([])
+        alert_config = self.env['reception.alert.config'].search([])
         if self.hr_alert_notification_count == 0 and self.elapsed_time > alert_config.hr_alert:
             self.ensure_one()
             self.reception_alert = alert_config
