e9d0ba2b047c94dc73f62244dde38dd128b8aaee
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-10 14:09:34 -0300

    route post para quality

--- dimabe_quality_integration/controllers/quality_analysis_controller.py
+++ dimabe_quality_integration/controllers/quality_analysis_controller.py
@@ -7,14 +7,12 @@ class QualityAnalysis(http.Controller):
     @http.route('/quality_analysis', type='json', auth='token', cors='*')
     def quality_analysis_list(self):
         res = request.env['quality.analysis'].sudo().search([])
-        exceptions._logger.error('controller')
-
         return res.read([
             'pre_caliber',
             'caliber_ids'
         ])
 
-    @http.route('quality_analysis', type='json', auth='token', cors='*', methods=['POST'])
+    @http.route('/quality_analysis', type='json', auth='token', cors='*', methods=['POST'])
     def quality_analysis_post(self):
         return {
             'ok': 'ok'
