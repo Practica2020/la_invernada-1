a0a42598a05811a5c2ce522980e4db520ed55d8a
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-10 10:55:58 -0300

    reset de modulo jwt dimabe

--- dimabe_jwt_token/models/ir_http.py
+++ dimabe_jwt_token/models/ir_http.py
@@ -6,23 +6,23 @@ import jwt
 class ItHttp(models.AbstractModel):
     _inherit = 'ir.http'
 
-    def _auth_method_token():
-        # raise exceptions.AccessDenied()
-        token = request.httprequest.headers.get('authorization', '', type=str)
-        if token:
-            token = token.split(' ')[1]
-            try:
-                payload = jwt.decode(
-                    token,
-                    'skjdfe48ueq893rihesdio*($U*WIO$u8',
-                    algorithms=['HS256']
-                )
-                if 'sub' in payload:
-                    u = request.env['res.users'].sudo().search(
-                        [('id', '=', 2)]
-                    )
-                    exceptions._logger.error('AAAAA {}'.format(u))
-            except jwt.ExpiredSignatureError:
-                raise exceptions.AccessDenied()
-            exceptions._logger.error('AAAAA {}'.format(request.env.user))
-        print('')
+    # def _auth_method_token():
+    #     # raise exceptions.AccessDenied()
+    #     token = request.httprequest.headers.get('authorization', '', type=str)
+    #     if token:
+    #         token = token.split(' ')[1]
+    #         try:
+    #             payload = jwt.decode(
+    #                 token,
+    #                 'skjdfe48ueq893rihesdio*($U*WIO$u8',
+    #                 algorithms=['HS256']
+    #             )
+    #             if 'sub' in payload:
+    #                 u = request.env['res.users'].sudo().search(
+    #                     [('id', '=', 2)]
+    #                 )
+    #                 exceptions._logger.error('AAAAA {}'.format(u))
+    #         except jwt.ExpiredSignatureError:
+    #             raise exceptions.AccessDenied()
+    #         exceptions._logger.error('AAAAA {}'.format(request.env.user))
+    #     print('')
