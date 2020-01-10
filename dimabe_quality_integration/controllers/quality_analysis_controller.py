3a9cf44f3d1bc7bc7568b16c9ec465c2529a805b
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-10 12:23:09 -0300

    test it_http

--- dimabe_quality_integration/controllers/quality_analysis_controller.py
+++ dimabe_quality_integration/controllers/quality_analysis_controller.py
@@ -1,4 +1,4 @@
-from odoo import http
+from odoo import http, exceptions
 from odoo.http import request
 
 
@@ -7,6 +7,7 @@ class QualityAnalysis(http.Controller):
     @http.route('/quality_analysis', type='json', auth='token', cors='*')
     def quality_analysis_list(self):
         res = request.env['quality.analysis'].sudo().search([])
+        exceptions._logger.error('controller')
 
         return res.read([
             'pre_caliber',
