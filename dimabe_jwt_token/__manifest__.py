3e8a97c180c2d4ec72060f819ea0fcbdef60681e
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-10 10:55:58 -0300

    reset de modulo jwt dimabe

--- dimabe_jwt_token/__manifest__.py
+++ dimabe_jwt_token/__manifest__.py
@@ -25,11 +25,9 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+
     ],
 }
