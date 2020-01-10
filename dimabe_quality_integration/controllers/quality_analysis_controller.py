ef46df5c67eddb6012fde0b2b9dbefe7a24f2d93
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-10 14:11:23 -0300

    agrega GET a metodo para list

--- dimabe_quality_integration/controllers/quality_analysis_controller.py
+++ dimabe_quality_integration/controllers/quality_analysis_controller.py
@@ -4,7 +4,7 @@ from odoo.http import request
 
 class QualityAnalysis(http.Controller):
 
-    @http.route('/quality_analysis', type='json', auth='token', cors='*')
+    @http.route('/quality_analysis', type='json', auth='token', cors='*', methods=['GET'])
     def quality_analysis_list(self):
         res = request.env['quality.analysis'].sudo().search([])
         return res.read([
