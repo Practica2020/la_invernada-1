261915e1c60d6256167e8e6ab84751d9039bfa13
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-10 14:14:07 -0300

    test PST

--- dimabe_quality_integration/controllers/quality_analysis_controller.py
+++ dimabe_quality_integration/controllers/quality_analysis_controller.py
@@ -13,7 +13,8 @@ class QualityAnalysis(http.Controller):
         ])
 
     @http.route('/quality_analysis', type='json', auth='token', cors='*', methods=['POST'])
-    def quality_analysis_post(self):
+    def quality_analysis_post(self, quality_analysis):
         return {
-            'ok': 'ok'
+            'ok': 'ok',
+            'res': quality_analysis
         }
