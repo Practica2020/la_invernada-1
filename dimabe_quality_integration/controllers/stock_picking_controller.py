0fed7a4bfc62c18e402e2157f582dcbe415e0a2c
felipe caroca <felipe.caroca@dimabe.cl>
2020-01-13 17:44:12 -0300

    debug

--- dimabe_quality_integration/controllers/stock_picking_controller.py
+++ dimabe_quality_integration/controllers/stock_picking_controller.py
@@ -5,8 +5,8 @@ from odoo.http import request
 class StockPickingController(http.Controller):
 
     @http.route('/api/stock_picking', type='json', methods=['GET'], auth='token', cors='*')
-    def get_stock_picking(self, lot):
-        res = request.env['stock.picking'].search([('name', '=', lot)])
+    def get_stock_picking(self):
+        res = request.env['stock.picking'].search([('name', '=', 1)])
         if res:
             return res
         else:
