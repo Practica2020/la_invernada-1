0c50cdf5ec02a79d9a7bff0791240091ad209579
felipe caroca <felipe.caroca@dimabe.cl>
2020-01-14 16:42:16 -0300

    cambio a notfound

--- dimabe_quality_integration/controllers/quality_analysis_controller.py
+++ dimabe_quality_integration/controllers/quality_analysis_controller.py
@@ -27,7 +27,7 @@ class QualityAnalysis(http.Controller):
     @http.route('/api/quality_analysis', type='json', auth='token', cors='*', methods=['POST'])
     def quality_analysis_post(self, data):
         if 'lot' not in data:
-            raise werkzeug.exceptions.BadRequest('debe indicar lote')
+            raise werkzeug.exceptions.NotFound('debe indicar lote')
         lot = request.env['stock.production.lot'].search([('name', '=', data['lot'])])
         if not lot:
             raise werkzeug.exceptions.NotFound('lote no encontrado')
