6bd35b77061c19fdac1a7e19187f7376696f9830
felipe caroca <felipe.caroca@dimabe.cl>
2020-01-14 13:14:45 -0300

    completado de datos GET controller

--- dimabe_quality_integration/controllers/stock_picking_controller.py
+++ dimabe_quality_integration/controllers/stock_picking_controller.py
@@ -12,19 +12,19 @@ class StockPickingController(http.Controller):
         res = request.env['stock.picking'].search([('name', '=', lot)])
         if res:
             return {
-                'ProducerCode': 0,
+                'ProducerCode': res.partner_id.id,
                 'ProducerName': res.partner_id.name,
                 'VarietyName': res.get_mp_move().product_id.get_variety(),
                 'LotNumber': res.name,
                 'DispatchGuideNumber': res.guide_number,
                 'ReceptionDate': res.scheduled_date,
                 'ReceptionKgs': res.net_weight,
-                'DryKgs': '',
+                'ContainerTpe': res.get_canning_move().product_id.name,
                 'Season': res.scheduled_date.year,
-                'QualityNumber': '',
+                'tare': res.tare_weight,
                 'Warehouse': res.location_dest_id.name,
                 'QualityWeight': res.quality_weight,
-                'ContainerQuantity': '',
+                'ContainerQuantity': res.get_canning_move().quantity_done,
                 'ArticleDescription': res.get_mp_move().product_id.display_name,
                 'QualityGreenId': ''
             }
