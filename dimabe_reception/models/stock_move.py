afe7aea58966ae09766d738d64db3af68e693753
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-15 12:45:42 -0300

    debug

--- dimabe_reception/models/stock_move.py
+++ dimabe_reception/models/stock_move.py
@@ -49,7 +49,11 @@ class StockMove(models.Model):
                     for stock_move_line in stock_move.move_line_ids:
                         if stock_move.product_id.categ_id.is_mp:
                             total_qty = stock_move.picking_id.get_canning_move().product_uom_qty
-                            models._logger.error('aaaaaaaaaaaaaaaaaa {} {}'.format(total_qty, stock_move.name))
+                            models._logger.error('aaaaaaaaaaaaaaaaaa {} {} {}'.format(
+                                total_qty,
+                                stock_move_line.name,
+                                stock_move.picking_id.get_canning_move().name
+                            ))
                             calculated_weight = stock_move_line.qty_done / total_qty
                             if stock_move_line.lot_id:
 
