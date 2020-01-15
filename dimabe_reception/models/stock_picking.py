b05a2b4dcb6dcce0fca12797f4de7b8abe3cdb13
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-15 15:29:31 -0300

    debug

--- dimabe_reception/models/stock_picking.py
+++ dimabe_reception/models/stock_picking.py
@@ -204,7 +204,7 @@ class StockPicking(models.Model):
     def notify_alerts(self):
         alert_config = self.env['reception.alert.config'].search([])
         elapsed_datetime = datetime.strptime(self.elapsed_time, '%H:%M:%S')
-        raise models.ValidationError(elapsed_datetime.hour)
+        raise models._logger.error(elapsed_datetime.hour)
         if self.hr_alert_notification_count == 0 and self.elapsed_time > alert_config.hr_alert:
             self.ensure_one()
             self.reception_alert = alert_config
