236a12400fc72561c18c3c92c9aa94a6e757a810
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-17 10:43:10 -0300

    campo archivo etiqueta cliente en comex

--- modulo_test/models/stock_picking.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    client_label_file = fields.Binary(string='Archivo Etiqueta Cliente')
