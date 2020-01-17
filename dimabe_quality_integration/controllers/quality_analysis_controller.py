583b921f5a7c0ff0ef2e8fc9b88b682cc0545813
felipe caroca <felipe.caroca@dimabe.cl>
2020-01-17 16:05:40 -0300

    debug

--- dimabe_quality_integration/controllers/quality_analysis_controller.py
+++ dimabe_quality_integration/controllers/quality_analysis_controller.py
@@ -60,10 +60,10 @@ class QualityAnalysis(http.Controller):
     @http.route("/api/quality_analysis", type='json', auth='token', cors='*', methods=['PUT'])
     def put_lot(self, lot, data):
 
-        quality_analysis_ids = request['quality.analysis'].search([('lot_name', '=', lot)])
-        if quality_analysis_ids:
-            for quality_analysis in quality_analysis_ids:
-                quality_analysis.update(data)
+        # quality_analysis_ids = request['quality.analysis'].search([('lot_name', '=', lot)])
+        # if quality_analysis_ids:
+        #     for quality_analysis in quality_analysis_ids:
+        #         quality_analysis.update(data)
         return {
             'lot': lot,
             'data': data
