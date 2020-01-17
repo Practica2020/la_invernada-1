6aa0448dbcb4ca2bae7393b5b903a5bc3d659d52
felipe caroca <felipe.caroca@dimabe.cl>
2020-01-17 15:38:03 -0300

    debug

--- dimabe_quality_integration/controllers/stock_production_lot_controller.py
+++ dimabe_quality_integration/controllers/stock_production_lot_controller.py
@@ -1,19 +1,19 @@
-from odoo import http, exception, controller
-from odoo.http import request
-import werkzeug
-
-
-class StockProductionLotController(http.Controller):
-    
-    @http.route("/api/stock_production_lot",
-                cors='*',
-                auth='token',
-                methods=['PUT'],
-                type='json'
-                )
-    def put_lot(self):
-        
-        return {
-            'lot': 'lot.id'
-        }
+# from odoo import http, exception, controller
+# from odoo.http import request
+# import werkzeug
+#
+#
+# class StockProductionLotController(http.Controller):
+#
+#     @http.route("/api/stock_production_lot",
+#                 cors='*',
+#                 auth='token',
+#                 methods=['PUT'],
+#                 type='json'
+#                 )
+#     def put_lot(self):
+#
+#         return {
+#             'lot': 'lot.id'
+#         }
 
