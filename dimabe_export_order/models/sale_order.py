e22c72e0ac748cf6b21616cb4a2099d3c01b7b65
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-20 09:14:17 -0300

    debug

--- dimabe_export_order/models/sale_order.py
+++ dimabe_export_order/models/sale_order.py
@@ -1,7 +1,12 @@
-from odoo import fields, models
+from odoo import fields, models,api
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     contract_number = fields.Char('Contrato')
+
+    @api.model()
+    def __filter_clients(self):
+        if self.parent_company_type == 'person':
+            models._logger(self.parent_company_type)
