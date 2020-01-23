0acc3f912ec8e866f752ddf6d319da0b8acaa863
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-23 16:14:16 -0300

    tree view dispatched -change

--- dimabe_manufacturing/__manifest__.py
+++ dimabe_manufacturing/__manifest__.py
@@ -32,10 +32,10 @@
         'views/mrp_workorder.xml',
         'views/stock_production_lot_serial.xml',
         'views/stock_production_lot.xml',
-        'views/mrp_dispatched.xml',
         'views/mrp_production.xml',
         'reports/lot_serial_label_report.xml',
-        'views/views.xml'
+        'views/views.xml',
+        'views/mrp_dispatched.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
