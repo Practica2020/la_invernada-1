4e0b9af6e3993b7040c32fd03edc2bfb33c2b2d9
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-24 12:38:44 -0300

    tree view dispatched -test

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -15,6 +15,8 @@ class StockPicking(models.Model):
 
     required_loading_date = fields.Date(related='shipping_id.required_loading_date')
 
+    variety = fields.Many2one('product.attribute.custom.value','Variedad')
+
     contract_correlative = fields.Integer('corr')
 
     contract_correlative_view = fields.Char(
