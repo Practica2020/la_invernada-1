f98700600e7de6dacb0e2762949aa1c0d8df182d
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-27 09:09:19 -0300

    tree dispatched -add field

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -16,8 +16,6 @@ class StockPicking(models.Model):
     required_loading_date = fields.Date(
         related='shipping_id.required_loading_date')
 
-    weigth_packing = fields.Many2one('stock.move','Kilos')
-
     contract_correlative = fields.Integer('corr')
 
     contract_correlative_view = fields.Char(
