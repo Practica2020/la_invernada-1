a79bcf2ebe367989e576965eed6c05df9f752004
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-27 09:34:22 -0300

    tree dispatched -modify order

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -23,7 +23,6 @@ class StockPicking(models.Model):
         compute='_get_correlative_text'
     )
 
-    product = fields.Many2one('product')
 
     consignee_id = fields.Many2one(
         'res.partner',
