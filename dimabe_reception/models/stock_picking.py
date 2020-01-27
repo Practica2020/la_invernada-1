1269bd91196928e50a39f20371952de5c79d1996
leonardodimabe <leonardo.norambuena@dimabe.cl>
2020-01-27 11:23:55 -0300

    debug alert notify

--- dimabe_reception/models/stock_picking.py
+++ dimabe_reception/models/stock_picking.py
@@ -256,8 +256,8 @@ class StockPicking(models.Model):
 
     @api.multi
     def notify_alerts(self):
-        alert_config = self.env['reception.alert.config'].search([])
         models._logger.error('entró al metodo')
+        alert_config = self.env['reception.alert.config'].search([])
         elapsed_datetime = datetime.strptime(self.elapsed_time, '%H:%M:%S')
         if self.hr_alert_notification_count == 0 and elapsed_datetime.hour >= alert_config.hr_alert:
             self.ensure_one()
