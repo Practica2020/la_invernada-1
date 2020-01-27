16123d181bcb9f1d3977135c486e4bbb9284d9ab
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-27 15:21:55 -0300

    tree dispatched undo

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -18,7 +18,6 @@ class StockPicking(models.Model):
 
     variety = fields.Many2many(related="product_id.attribute_value_ids")
 
-    item = fields.Many2one('Item', compute='_get_product_variety')
 
     contract_correlative = fields.Integer('corr')
 
