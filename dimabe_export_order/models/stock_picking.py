37db4e664815422b98c561748e2a32960fe12c18
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-27 12:10:10 -0300

    tree dispatched -modify field

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -13,7 +13,7 @@ class StockPicking(models.Model):
         'Embarque'
     )
 
-    color = fields.Char('Color', compute='_get_product_variety')
+    variety = fields.Many2many(related="product_id.attribute_value_ids")
 
     required_loading_date = fields.Date(
         related='shipping_id.required_loading_date')
