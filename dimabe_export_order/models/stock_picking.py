9e7c5cd85b80a09f81ec252026310d759cab68a6
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-28 16:57:36 -0300

    tree dispatched

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -98,6 +98,7 @@ class StockPicking(models.Model):
 
     @api.multi
     def return_action(self):
+        models._logger.error('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa {}'.format(type(self.quantity_done)))
         return {
             "type": "ir.actions.act_window",
             "res_model":"mrp.production",
