c79ce43c93520460d1120aabac51b9d313d1e9da
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-29 11:22:27 -0300

    tree dispatched

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -100,7 +100,7 @@ class StockPicking(models.Model):
     @api.multi
     def return_action(self):
         qty = str(self.quantity_done).replace('.',',')
-        context = {'default_product_id':self.product.id,'default_product_qty':1000.0,'default_origin':self.name}
+        context = {'default_product_id':self.product.id,'default_product_qty':1000.000,'default_origin':self.name}
         models._logger.error(context)
         return {
             "type": "ir.actions.act_window",
