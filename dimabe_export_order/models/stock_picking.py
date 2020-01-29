ca26be370409203e1a1d02edddad64cf4e9c7f01
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-29 10:29:17 -0300

    tree dispatched

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -101,7 +101,8 @@ class StockPicking(models.Model):
     def return_action(self):
         qty = str(self.quantity_done)
         product_qty = qty.replace('.0', ',0')
-        models._logger.error('6666666666666666666666666666666666666666666666666{} '.format(product_qty))
+        qty_done = int(product_qty)
+        models._logger.error('6666666666666666666666666666666666666666666666666{} '.format(qty_done))
         return {
             "type": "ir.actions.act_window",
             "res_model": "mrp.production",
