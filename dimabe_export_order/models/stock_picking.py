a3ac0d7ed64b24fb35555eb91f6116f3c99a20f1
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-29 09:27:55 -0300

    tree dispatched

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -110,7 +110,7 @@ class StockPicking(models.Model):
             "views": [(False, "form")],
             "view_id ref='mrp.mrp_production_form_view'": '',
             "target": "new",
-            "context": "{'default_product_id':"+str(self.product.id)+",'default_product_qty':1000,0}"
+            "context": "{'default_product_id':"+str(self.product.id)+"}"
         }
 
     @api.model
