600659d7d4077a0f9a47f8ea74227e6065d2adf6
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-30 09:03:45 -0300

    test page

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -4,9 +4,9 @@ from odoo import fields, models, api
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    serie = fields.Many2many("stock.production.lot")
+    test = fields.Many2one('stock.production.lot')
 
-    lots = fields.One2many(rel="serie.stock_production_lot_serial_ids")
+    
 
     @api.multi
     def calculate_done(self):
