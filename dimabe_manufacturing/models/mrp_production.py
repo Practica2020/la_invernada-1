a00bcf6f526a6b87655cf953d565ea002e134e2a
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-30 08:30:06 -0300

    test page

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -6,7 +6,7 @@ class MrpProduction(models.Model):
 
     lots = fields.Many2one('stock.production.lot')
 
-    product_lot = fields.Many2one(rel='lots.product_id')
+    product_lot = fields.Char(rel='lots.product_id')
 
     @api.multi
     def calculate_done(self):
