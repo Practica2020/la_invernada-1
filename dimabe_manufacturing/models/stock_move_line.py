7acc48db8ac4f10c16bade6685cdc69acfe1b0e4
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-30 16:43:20 -0300

    test page

--- dimabe_manufacturing/models/stock_move_line.py
+++ dimabe_manufacturing/models/stock_move_line.py
@@ -4,6 +4,10 @@ import logging
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
+    lots = fields.Many2one("mrp.production")
+    
+    lots_id = fields.Integer(rel="lots.id")
+
     count_stock_production_lot_serial = fields.Integer(
         'Total Bultos',
         compute='_compute_count_stock_production_lot_serial'
@@ -12,5 +16,6 @@ class StockMoveLine(models.Model):
     @api.multi
     def _compute_count_stock_production_lot_serial(self):
         for item in self:
+            models._logger.error(self.lots_id)
             if item.lot_id:
                 item.count_stock_production_lot_serial = len(item.lot_id.stock_production_lot_serial_ids)
