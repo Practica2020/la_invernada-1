a8626545d0c83ff10ef0f0a3008c806bd2cf1538
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-01-31 09:41:32 -0300

    test -fix typo error

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -9,13 +9,13 @@ class MrpProduction(models.Model):
     
     serial_lot_ids = fields.One2many(related="stock_lots.stock_production_lot_serial_ids")
 
-    @api.onchange('product_id')
-    def onchange_stock_lots(self):
-        if self.product_id:
-            models._logger.error('product_id {}'.format(self.product_id))
-            res = {}
-            res['domain']={'stock_lots':["product_id","=",self.product_id]}
-            return res
+    # @api.onchange('product_id')
+    # def onchange_stock_lots(self):
+    #     if self.product_id:
+    #         models._logger.error('product_id {}'.format(self.product_id))
+    #         res = {}
+    #         res['domain']={'stock_lots':["product_id","=",self.product_id]}
+    #         return res
 
     @api.multi
     def calculate_done(self):
