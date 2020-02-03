9b883d3f4524f11424a549fb6c934492e2795f1f
dimabe-odoo <54315923+dimabe-odoo@users.noreply.github.com>
2020-02-03 08:53:20 -0300

    set stock_lots invisible

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -99,7 +99,7 @@ class StockPicking(models.Model):
 
     @api.multi
     def return_action(self):
-        context= {'default_product_id':self.product.id,'default_product_uom_qty':self.quantity_done,'default_origin':self.origin}
+        context= {'default_product_id':self.product.id,'default_product_uom_qty':self.quantity_done,'default_origin':self.name}
         return {
             "type": "ir.actions.act_window",
             "res_model": "mrp.production",
