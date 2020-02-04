f6066699e4d9682d496c099481b1dd42d6c50ede
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-04 08:31:59 -0300

    debug

--- dimabe_manufacturing/models/stock_picking.py
+++ dimabe_manufacturing/models/stock_picking.py
@@ -13,7 +13,7 @@ class StockPicking(models.Model):
             'default_origin': self.name,
             'default_product_qty': self.quantity_done,
             'default_related_picking_id': self.id,
-            'default_picking_ids': [(6, 0, self.id)]
+            'default_picking_ids': [(6, 0, [self.id])]
         }
 
         return {
