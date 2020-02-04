4c2a3fe3c1ba804d7f10ff97a59fee710cb7945a
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-04 09:44:37 -0300

    debug

--- dimabe_quality_integration/models/stock_picking.py
+++ dimabe_quality_integration/models/stock_picking.py
@@ -5,10 +5,11 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     stock_move_line_lot_ids = fields.One2many(
+        'stock.move.line',
         string='Detalle',
-        compute= '_compute_get_mp_move_line'
+        compute='_compute_stock_move_line_lot_ids'
     )
 
-    def _compute_get_mp_move_line(self):
+    def _compute_stock_move_line_lot_ids(self):
         if self.get_mp_move():
             self.stock_move_line_lot_ids = self.get_mp_move().move_line_ids
