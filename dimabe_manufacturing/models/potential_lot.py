596ee56c951c0191c4c0b497ae48cd5fe43226e7
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-06 15:35:34 -0300

    potential_lot

--- dimabe_manufacturing/models/potential_lot.py
+++ dimabe_manufacturing/models/potential_lot.py
@@ -17,6 +17,11 @@ class PotentialLot(models.Model):
         related='stock_production_lot_id.available_quantity'
     )
 
+    stock_quant = fields.Many2one(
+        'stock.quant',
+        related='stock_production_lot.stock_quant'
+    )
+
     stock_production_lot_id = fields.Many2one('stock.production.lot', 'lote potencial')
 
     mrp_production_id = fields.Many2one('mrp.production', 'Producción')
