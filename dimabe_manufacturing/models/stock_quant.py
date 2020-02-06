59ccf37873fb55f53c340fc98992db37c830a4ca
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-06 15:49:58 -0300

    potential_lot

--- dimabe_manufacturing/models/stock_quant.py
+++ dimabe_manufacturing/models/stock_quant.py
@@ -6,10 +6,12 @@ class StockQuant(models.Model):
 
     balance = fields.Float(
         'Saldo',
-        compute='_compute_balance'
+        compute='_compute_balance',
+        store=True
     )
 
     @api.multi
+    @api.depends('quantity', 'reserved_quantity')
     def _compute_balance(self):
         for item in self:
             item.balance = item.quantity - item.reserved_quantity
