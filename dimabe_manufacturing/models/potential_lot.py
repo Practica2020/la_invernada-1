cc0eb630ce2be80019e26bd7f28bf984dbdee195
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-06 12:40:10 -0300

    potential_lot

--- dimabe_manufacturing/models/potential_lot.py
+++ dimabe_manufacturing/models/potential_lot.py
@@ -59,6 +59,6 @@ class PotentialLot(models.Model):
 
         move_line = stock_move.active_move_line_ids.filtered(lambda a: a.lot_id == self.stock_production_lot_id.id)
 
-        raise models.ValidationError(move_line)
+        raise models.ValidationError(stock_move)
 
         self.is_reserved = False
