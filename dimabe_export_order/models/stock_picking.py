e80547b49d9e41ff829f9fa67c86a59895d38a8a
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-10 11:16:43 -0300

    test

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -99,7 +99,7 @@ class StockPicking(models.Model):
 
     picture = fields.Many2many("ir.attachment")
 
-
+    picture_ext = fields.Many2one(rel="picture_mimetype")
 
     @api.model
     def _get_product_variety(self):
