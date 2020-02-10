965e737fdf64ab4c5c508bb3300829112d16b431
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-10 08:47:38 -0300

    debug

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -129,9 +129,9 @@ class MrpProduction(models.Model):
 
             return res
 
-    # def _workorders_create(self, bom, bom_data):
-    #
-    #     raise models.ValidationError('{}---{}'.format(bom, bom_data))
-    #
-    #
-    #     return super(MrpProduction, self)._workorders_create(bom, bom_data)
+    def _workorders_create(self, bom, bom_data):
+
+        raise models.ValidationError('{}---{}'.format(bom, bom_data))
+
+
+        return super(MrpProduction, self)._workorders_create(bom, bom_data)
