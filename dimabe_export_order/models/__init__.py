9debf1d80d477442c5a01bbeb0689b76d3c3fe10
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-10 14:23:53 -0300

    get data fname

--- dimabe_export_order/models/__init__.py
+++ dimabe_export_order/models/__init__.py
@@ -11,4 +11,3 @@ from . import sale_order
 from . import stock_picking
 from . import stock_move
 from . import custom_report
-from . import ir_attachment
--- dimabe_export_order/models/ir.attachment.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-
-class IrAttachment(models.Model):
-    _inherit = 'ir.attachment'
-
-    file = fields.Binary("Attachment")
-    file_name = fields.Char("File Name",rel="data_fname")
