626308df78433d2f7d51fd4a4d6b7c8a3501d637
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-10 10:04:47 -0300

    test many2many fields

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -97,7 +97,7 @@ class StockPicking(models.Model):
         'Tipo de contenedor'
     )
 
-    picture = fields.Many2many('custom.report')
+    picture = fields.Many2many(comodel_name="ir.attachment")
 
     @api.model
     def _get_product_variety(self):
