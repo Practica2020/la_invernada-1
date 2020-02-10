cbb30ef01877880ae320f71c9c12b50f5d496f91
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-10 15:25:46 -0300

    debug filtro

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -38,7 +38,7 @@ class MrpProduction(models.Model):
             ('name', 'not in', list(self.potential_lot_ids.mapped('stock_production_lot_id.id')))
         ]
 
-        raise models.ValidationError(self.procurement_group_id)
+        raise models.ValidationError(self.procurement_group_id.sale_id)
 
         if self.client_search_id:
 
