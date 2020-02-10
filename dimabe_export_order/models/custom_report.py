02e6bedf609b0bed659abb496c4e967a1e4ec889
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-10 14:27:06 -0300

    change in custom_report

---
+++ dimabe_export_order/models/custom_report.py
@@ -0,0 +1,9 @@
+from odoo import models, fields,api
+
+
+class CustomReport(models.Model):
+    _name="custom.report"
+    _inherit = 'ir.attachment'
+
+    file = fields.Binary("Attachment")
+    file_name = fields.Char("File Name",related='ir.attachment.data_fname')
