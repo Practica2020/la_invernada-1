4a9f2535a66d49ba70b61387165a0aa9e88aee1f
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-10 17:14:57 -0300

    test

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -105,8 +105,7 @@ class StockPicking(models.Model):
     def generate_data(self):
         test = list(self.picture.mapped("local_url"))
         for item in test:
-            return self.env.ref('dimabe_export_order.action_dispatch_label_report') \
-                .report_action(item)
+            models._logger.error(item)
 
     @api.model
     def _get_product_variety(self):
