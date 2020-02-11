e3c4c34fdb67aa910961901622f74cde08b6c295
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-11 08:47:19 -0300

    test dispatch_report export_order

---
+++ dimabe_export_order/models/ir.attachment.py
@@ -0,0 +1,10 @@
+from odoo import models,fields,api
+
+class IrAttachment(models.Model):
+    _inherit = "ir.attachment"
+
+    @api.multi
+    def get_full_url(self):
+        self.ensure_one()
+        base_url = self.env["ir.config_parameter"].sudo().get_param("web.base.url")
+        return base_url
