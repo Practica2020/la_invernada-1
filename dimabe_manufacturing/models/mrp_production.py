df9a8786ccd39e973e3b11b2b896da65d295613b
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-11 08:35:42 -0300

    debug

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -48,119 +48,119 @@ class MrpProduction(models.Model):
             raise models.ValidationError(filtered_lot_ids)
 
 
-@api.model
-def get_potential_lot_ids(self):
-    potential_lot_ids = []
+    @api.model
+    def get_potential_lot_ids(self):
+        potential_lot_ids = []
 
-    domain = [
-        ('product_id', 'in', list(self.move_raw_ids.mapped('product_id.id'))),
-        # ('name', 'not in', list(self.potential_lot_ids.mapped('stock_production_lot_id.name')))
-    ]
+        domain = [
+            ('product_id', 'in', list(self.move_raw_ids.mapped('product_id.id'))),
+            # ('name', 'not in', list(self.potential_lot_ids.mapped('stock_production_lot_id.name')))
+        ]
 
-    if self.client_search_id:
-        client_lot_ids = self.env['quality.analysis'].search([
-            ('potential_client_id', '=', self.client_search_id.id)
-        ]).mapped('stock_production_lot_ids.name')
+        if self.client_search_id:
+            client_lot_ids = self.env['quality.analysis'].search([
+                ('potential_client_id', '=', self.client_search_id.id)
+            ]).mapped('stock_production_lot_ids.name')
 
-        domain += [('name', 'in', list(client_lot_ids) if client_lot_ids else [])]
-        models._logger.error(domain)
+            domain += [('name', 'in', list(client_lot_ids) if client_lot_ids else [])]
+            models._logger.error(domain)
 
-    res = self.env['stock.production.lot'].search(domain)
+        res = self.env['stock.production.lot'].search(domain)
 
-    for pl in res:
-        if pl.stock_quant_balance > 0:
-            pl.lot_available_quantity = pl.stock_quant_balance
-            potential_lot_ids.append(pl)
+        for pl in res:
+            if pl.stock_quant_balance > 0:
+                pl.lot_available_quantity = pl.stock_quant_balance
+                potential_lot_ids.append(pl)
 
-    return [{
-        'stock_production_lot_id': lot.id,
-        'mrp_production_id': self.id,
-        'lot_available_quantity': lot.stock_quant_balance
-    } for lot in potential_lot_ids]
-
-
-@api.multi
-def set_stock_move(self):
-    product = self.env['stock.move'].create({'product_id': self.product_id})
-    product_qty = self.env['stock.move'].create({'product_qty': self.product_qty})
-    self.env.cr.commit()
+        return [{
+            'stock_production_lot_id': lot.id,
+            'mrp_production_id': self.id,
+            'lot_available_quantity': lot.stock_quant_balance
+        } for lot in potential_lot_ids]
 
 
-@api.multi
-def calculate_done(self):
-    for item in self:
-        for line_id in item.finished_move_line_ids:
-            line_id.qty_done = line_id.lot_id.total_serial
+    @api.multi
+    def set_stock_move(self):
+        product = self.env['stock.move'].create({'product_id': self.product_id})
+        product_qty = self.env['stock.move'].create({'product_qty': self.product_qty})
+        self.env.cr.commit()
 
 
-@api.multi
-def button_mark_done(self):
-    self.calculate_done()
-    return super(MrpProduction, self).button_mark_done()
+    @api.multi
+    def calculate_done(self):
+        for item in self:
+            for line_id in item.finished_move_line_ids:
+                line_id.qty_done = line_id.lot_id.total_serial
 
 
-@api.model
-def create(self, values_list):
-    res = super(MrpProduction, self).create(values_list)
-
-    regs = [
-        (0, 0, potential_lot) for potential_lot in res.get_potential_lot_ids()
-    ]
-
-    res.update({
-        'potential_lot_ids': regs})
-
-    stock_picking = self.env['stock.picking'].search([
-        ('name', '=', res.origin)
-    ])
-
-    if stock_picking:
-        stock_picking.update({
-            'has_mrp_production': True
-        })
-
-    return res
+    @api.multi
+    def button_mark_done(self):
+        self.calculate_done()
+        return super(MrpProduction, self).button_mark_done()
 
 
-@api.multi
-def button_plan(self):
-    for order in self:
-        # if sum(order.move_raw_ids.filtered(lambda a: a.is_mp).mapped('reserved_availability')) < order.product_qty:
-        #     raise models.ValidationError('la cantidad a consumir no puede ser menor a la cantidad a producir')
+    @api.model
+    def create(self, values_list):
+        res = super(MrpProduction, self).create(values_list)
 
-        for stock_move in order.move_raw_ids:
-            stock_move.product_uom_qty = stock_move.reserved_availability
-            stock_move.unit_factor = stock_move.product_uom_qty / order.product_qty
-            if stock_move.product_uom_qty == 0:
-                stock_move.update({
-                    'raw_material_production_id': None
-                })
-        order.move_raw_ids = order.move_raw_ids.filtered(
-            lambda a: a.raw_material_production_id.id == order.id
-        )
+        regs = [
+            (0, 0, potential_lot) for potential_lot in res.get_potential_lot_ids()
+        ]
 
-        # real_bom_data = []
-        # real_product_qty = order.bom_id.product_qty
-        #
-        # order.bom_id.product_qty = order.product_uom_id._compute_quantity(order.product_qty,
-        #                                                                    order.bom_id.product_uom_id)
-        #
-        # for bom_line in order.bom_id.bom_line_ids:
-        #     raw_line = order.move_raw_ids.filtered(lambda a: a.product_id == bom_line.product_id)
-        #     if raw_line:
-        #         real_bom_data.append({
-        #             'product_id': bom_line.product_id,
-        #             'product_qty': bom_line.product_qty
-        #         })
-        #         bom_line.product_qty = raw_line.product_uom_qty
+        res.update({
+            'potential_lot_ids': regs})
 
-        res = super(MrpProduction, order).button_plan()
+        stock_picking = self.env['stock.picking'].search([
+            ('name', '=', res.origin)
+        ])
 
-        # for rd in real_bom_data:
-        #     bl = order.bom_id.bom_line_ids.filtered(lambda a: a.product_id == rd['product_id'])
-        #     if bl:
-        #         bl.product_qty = rd['product_qty']
-        #
-        # order.bom_id.product_qty = real_product_qty
+        if stock_picking:
+            stock_picking.update({
+                'has_mrp_production': True
+            })
 
         return res
+
+
+    @api.multi
+    def button_plan(self):
+        for order in self:
+            # if sum(order.move_raw_ids.filtered(lambda a: a.is_mp).mapped('reserved_availability')) < order.product_qty:
+            #     raise models.ValidationError('la cantidad a consumir no puede ser menor a la cantidad a producir')
+
+            for stock_move in order.move_raw_ids:
+                stock_move.product_uom_qty = stock_move.reserved_availability
+                stock_move.unit_factor = stock_move.product_uom_qty / order.product_qty
+                if stock_move.product_uom_qty == 0:
+                    stock_move.update({
+                        'raw_material_production_id': None
+                    })
+            order.move_raw_ids = order.move_raw_ids.filtered(
+                lambda a: a.raw_material_production_id.id == order.id
+            )
+
+            # real_bom_data = []
+            # real_product_qty = order.bom_id.product_qty
+            #
+            # order.bom_id.product_qty = order.product_uom_id._compute_quantity(order.product_qty,
+            #                                                                    order.bom_id.product_uom_id)
+            #
+            # for bom_line in order.bom_id.bom_line_ids:
+            #     raw_line = order.move_raw_ids.filtered(lambda a: a.product_id == bom_line.product_id)
+            #     if raw_line:
+            #         real_bom_data.append({
+            #             'product_id': bom_line.product_id,
+            #             'product_qty': bom_line.product_qty
+            #         })
+            #         bom_line.product_qty = raw_line.product_uom_qty
+
+            res = super(MrpProduction, order).button_plan()
+
+            # for rd in real_bom_data:
+            #     bl = order.bom_id.bom_line_ids.filtered(lambda a: a.product_id == rd['product_id'])
+            #     if bl:
+            #         bl.product_qty = rd['product_qty']
+            #
+            # order.bom_id.product_qty = real_product_qty
+
+            return res
