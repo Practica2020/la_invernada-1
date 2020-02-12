2ab2f3d5208b9db2d876e7fb2901fd391e377e51
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-12 10:31:43 -0300

    debug

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -53,6 +53,7 @@ class MrpProduction(models.Model):
                 if not production.potential_lot_ids.filtered(
                         lambda a: a.stock_production_lot_id.id == filtered_lot_id['stock_production_lot_id']
                 ):
+                    models._logger.error(filtered_lot_id)
                     to_add.append((0, 0, filtered_lot_id))
 
             production.update({
