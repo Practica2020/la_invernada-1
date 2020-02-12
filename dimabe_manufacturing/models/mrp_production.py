28cdf488e06030f6fc0922cf3609b9806711fdf4
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-12 12:35:14 -0300

    debug

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -54,16 +54,14 @@ class MrpProduction(models.Model):
                 if not production.potential_lot_ids.filtered(
                         lambda a: a.stock_production_lot_id.id == filtered_lot_id['stock_production_lot_id']
                 ):
-                    models._logger.error('{} {} {}'.format(
-                        production.potential_lot_ids,
-                        production.potential_lot_ids.mapped('stock_production_lot_id'),
-                        filtered_lot_id['stock_production_lot_id']))
-
-                    # tmp_id = self.env['potential.lot'].create(filtered_lot_id)
-                    # to_add.append((4, tmp_id.id))
+                    to_add.append(filtered_lot_id)
+            to_add_processed = []
+            for new_add in to_add:
+                tmp_id = self.env['potential.lot'].create(new_add)
+                to_add_processed.append((4, tmp_id.id))
 
             production.update({
-                'potential_lot_ids': to_add + to_keep
+                'potential_lot_ids': to_add_processed + to_keep
             })
 
     @api.model
