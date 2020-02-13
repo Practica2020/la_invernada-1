569ef83d811f220ddced69c8de7563b992e8e242
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-13 09:43:15 -0300

    debug

--- dimabe_manufacturing/models/mrp_production.py
+++ dimabe_manufacturing/models/mrp_production.py
@@ -67,7 +67,7 @@ class MrpProduction(models.Model):
 
     @api.model
     def get_potential_lot_ids(self):
-        domain = []
+        domain = [('balance', '>', 0)]
         res = []
         if self.client_search_id:
             client_lot_ids = self.env['quality.analysis'].search([
