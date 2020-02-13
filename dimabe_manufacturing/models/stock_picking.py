13b16bb62d066bc4c4b7f6772b42622f4dd1e676
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-13 11:23:36 -0300

    test

--- dimabe_manufacturing/models/stock_picking.py
+++ dimabe_manufacturing/models/stock_picking.py
@@ -20,33 +20,34 @@ class StockPicking(models.Model):
 
     product_id = fields.Many2one(related="sale_id.order_line.product_id")
 
-#    quantity_requested = fields.Float(related='sale_id.order_line.product_uom_qty')
+    quantity_requested = fields.Float(related='sale_id.order_line.product_uom_qty')
 
-    @api.multi
-    def return_action(self):
-        procurement_group = self.env['procurement.group'].search([
-            ('name', '=', self.origin)
-        ])
 
-        if procurement_group:
-            procurement_group = procurement_group[0]
+@api.multi
+def return_action(self):
+    procurement_group = self.env['procurement.group'].search([
+        ('name', '=', self.origin)
+    ])
 
-        context = {
-            'default_product_id': self.product_id.id,
-            'default_product_uom_qty': self.quantity_requested,
-            'default_origin': self.name,
-            'default_procurement_group_id': procurement_group.id,
-            'default_client_search_id': self.partner_id.id,
-            'default_requested_qty': self.quantity_requested
-        }
+    if procurement_group:
+        procurement_group = procurement_group[0]
 
-        return {
-            "type": "ir.actions.act_window",
-            "res_model": "mrp.production",
-            "view_type": "form",
-            "view_mode": "form",
-            "views": [(False, "form")],
-            "view_id ref='mrp.mrp_production_form_view'": '',
-            "target": "current",
-            "context": context
-        }
+    context = {
+        'default_product_id': self.product_id.id,
+        'default_product_uom_qty': self.quantity_requested,
+        'default_origin': self.name,
+        'default_procurement_group_id': procurement_group.id,
+        'default_client_search_id': self.partner_id.id,
+        'default_requested_qty': self.quantity_requested
+    }
+
+    return {
+        "type": "ir.actions.act_window",
+        "res_model": "mrp.production",
+        "view_type": "form",
+        "view_mode": "form",
+        "views": [(False, "form")],
+        "view_id ref='mrp.mrp_production_form_view'": '',
+        "target": "current",
+        "context": context
+    }
