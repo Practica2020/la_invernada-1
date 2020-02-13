7d199fb70f67786606d46301f0849a1456313a8c
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-13 09:00:26 -0300

    debug

--- dimabe_manufacturing/models/mrp_dispatched.py
+++ dimabe_manufacturing/models/mrp_dispatched.py
@@ -5,19 +5,4 @@ class MrpDispatched(models.Model):
     _name = 'mrp.dispatched'
     _inherit = 'stock.picking'
 
-    shipping_id = fields.Many2one(
-        'custom.shipment',
-        'Embarque'
-    )
 
-    required_loading_date = fields.Date(
-        related='shipping_id.required_loading_date')
-
-    variety = fields.Many2many(related="product_id.attribute_value_ids")
-
-    country = fields.Char(related='partner_id.country_id.name')
-
-    quantity_done = fields.Float(
-        related='product.product_uom_qty')
-
-    product = fields.Many2one(related="sale_id.order_line.product_id")
