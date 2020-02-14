481da67338958e8954c1a24c20fb23487a969e65
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-14 08:23:55 -0300

    test

--- dimabe_purchase_process/models/purchase_order.py
+++ dimabe_purchase_process/models/purchase_order.py
@@ -44,7 +44,7 @@ class PurchaseOrder(models.Model):
     @api.multi
     def action_rfq_send(self):
         for item in self:
-            models._logger.error("3333333333333333333333333333333333 {}".format(item))
+            models._logger.error("3333333333333333333333333333333333 {}".format(item.state))
             if not item.boss_approval_id:
                 models._logger.error("3333333333333333333333333333333333 {}".format(item))
                 item.update({
