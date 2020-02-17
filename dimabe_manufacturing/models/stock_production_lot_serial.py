352f58e93d68f3b5ca8a9ca5c2e580488dda8347
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-17 10:52:45 -0300

    debug

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -62,7 +62,7 @@ class StockProductionLotSerial(models.Model):
                     'reserved_to_production_id': production.id
                 })
                 # item.consumed = True
-                raise models.ValidationError(production)
+
                 stock_move = item.production_id.move_raw_ids.filtered(
                     lambda a: a.product_id == item.stock_production_lot_id.product_id
                 )
