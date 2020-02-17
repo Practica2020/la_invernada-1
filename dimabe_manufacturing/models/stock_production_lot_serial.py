5f5ece57a521028f404ce0999a49e4f006d8ee3b
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-17 11:51:55 -0300

    debug

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -64,44 +64,43 @@ class StockProductionLotSerial(models.Model):
     def reserve_serial(self):
         if 'params' in self.env.context and 'id' in self.env.context['params']:
             production_id = self.env.context['params']['id']
-            raise models.ValidationError(production_id)
             production = self.env['mrp.production'].search([('id', '=', production_id)])
             if not production:
                 raise models.ValidationError('No se encontró la orden de producción a la que reservar el producto')
-            for item in self:
-                item.update({
-                    'reserved_to_production_id': production.id
-                })
+            # for item in self:
+            self.update({
+                'reserved_to_production_id': production.id
+            })
 
-                stock_move = production.move_raw_ids.filtered(
-                    lambda a: a.product_id == item.stock_production_lot_id.product_id
-                )
+            stock_move = production.move_raw_ids.filtered(
+                lambda a: a.product_id == self.stock_production_lot_id.product_id
+            )
 
-                stock_quant = item.stock_production_lot_id.quant_ids.filtered(
-                    lambda a: a.location_id.name == 'Stock'
-                )
+            stock_quant = self.stock_production_lot_id.quant_ids.filtered(
+                lambda a: a.location_id.name == 'Stock'
+            )
 
-                virtual_location_production_id = item.env['stock.location'].search([
-                    ('usage', '=', 'production'),
-                    ('location_id.name', 'like', 'Virtual Locations')
-                ])
+            virtual_location_production_id = self.env['stock.location'].search([
+                ('usage', '=', 'production'),
+                ('location_id.name', 'like', 'Virtual Locations')
+            ])
 
-                stock_quant.sudo().update({
-                    'reserved_quantity': stock_quant.reserved_quantity + item.display_weight
-                })
+            stock_quant.sudo().update({
+                'reserved_quantity': stock_quant.reserved_quantity + self.display_weight
+            })
 
-                stock_move.sudo().update({
-                    'active_move_line_ids': [
-                        (0, 0, {
-                            'product_id': item.stock_production_lot_id.product_id.id,
-                            'lot_id': item.stock_production_lot_id.id,
-                            'product_uom_qty': item.display_weight,
-                            'product_uom_id': stock_move.product_uom.id,
-                            'location_id': stock_quant.location_id.id,
-                            'location_dest_id': virtual_location_production_id.id
-                        })
-                    ]
-                })
+            stock_move.sudo().update({
+                'active_move_line_ids': [
+                    (0, 0, {
+                        'product_id': self.stock_production_lot_id.product_id.id,
+                        'lot_id': self.stock_production_lot_id.id,
+                        'product_uom_qty': self.display_weight,
+                        'product_uom_id': stock_move.product_uom.id,
+                        'location_id': stock_quant.location_id.id,
+                        'location_dest_id': virtual_location_production_id.id
+                    })
+                ]
+            })
 
         #     item.is_reserved = True
 
