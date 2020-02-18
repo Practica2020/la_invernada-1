057a6d98daac13510df75408cc57b1ed4a11691a
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-18 16:17:17 -0300

    debug

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -53,6 +53,7 @@ class StockProductionLotSerial(models.Model):
     @api.multi
     def reserve_serial(self):
         models._logger.error(self.env.context)
+        models._logger.error(self.env)
         if 'production_id' in self.env.context:
             raise models.ValidationError(self.env.context['production_id'])
         if 'params' in self.env.context and 'id' in self.env.context['params']:
