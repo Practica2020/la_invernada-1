c2d7d01a8f512c12663875e1e98be14e8604b5a2
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-19 12:28:59 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -137,6 +137,7 @@ class MrpWorkorder(models.Model):
 
             if not lot_search:
                 raise models.ValidationError('no se encontró registro asociado al código ingresado')
+
             if not lot_search.product_id.categ_id.reserve_ignore:
                 raise models.ValidationError(
                     'el código escaneado no se encuentra dentro de la planificación de esta producción'
