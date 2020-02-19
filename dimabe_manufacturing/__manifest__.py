2e9052c13ff0eb8568ecbbf1c58754a5278b89d0
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-19 09:30:38 -0300

    debug test js

--- dimabe_manufacturing/__manifest__.py
+++ dimabe_manufacturing/__manifest__.py
@@ -20,6 +20,7 @@
     # any module necessary for this one to work correctly
     'depends': [
         'base',
+        'website',
         'dimabe_reception',
         'mrp',
         'mrp_workorder',
@@ -40,7 +41,8 @@
         'views/mrp_workcenter.xml',
         'views/quality_analysis.xml',
         'views/product_category.xml',
-        'views/potential_lot.xml'
+        'views/potential_lot.xml',
+        'views/templates.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
---
+++ dimabe_manufacturing/static/src/js/production_tablet_view.js
@@ -0,0 +1,7 @@
+odoo.define('lala', function (require) {
+    let core = require('web.core');
+    alert(core._t('ase noma'));
+    return {
+
+    }
+})
