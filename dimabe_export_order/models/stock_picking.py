07c91314ac32ce6bc2db63373bbd2ec5011949f2
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-20 17:58:57 -0300

    test

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -152,7 +152,6 @@ class StockPicking(models.Model):
     def get_type_of_transfer(self):
         self.type_of_transfer = dict(self._fields['type_of_transfer_list'].selection).get(self.type_of_transfer_list)
         models._logger.error(self.env.user.groups_id)
-        test = dict(self.env.user.groups_id)
         for t in self.env.user.groups_id:
             models._logger.error(t.name)
         return self.type_of_transfer
