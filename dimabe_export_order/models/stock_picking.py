195556a5536597ed8f2615feb3289b2946c5d54f
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-20 17:17:56 -0300

    test

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -149,10 +149,10 @@ class StockPicking(models.Model):
 
     @api.multi
     def get_type_of_transfer(self):
-        type_of_transfer = dict(self._fields['type_of_transfer_list'].selection).get(self.type_of_transfer_list)
+        self.type_of_transfer = dict(self._fields['type_of_transfer_list'].selection).get(self.type_of_transfer_list)
         models._logger.error(dict(self._fields['type_of_transfer_list'].selection).get(self.type_of_transfer_list))
-        models._logger.error(type_of_transfer)
-        return type_of_transfer
+        models._logger.error(self.type_of_transfer)
+        return self.type_of_transfer
 
     @api.multi
     def get_full_url(self):
