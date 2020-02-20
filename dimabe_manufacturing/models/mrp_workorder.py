1813cd6ded15c4993f070b1b2dc7526784d1f16b
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-20 12:39:23 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -105,8 +105,9 @@ class MrpWorkorder(models.Model):
                     check.lot_id = lot_tmp.id
                     if not self.lot_id:
                         self.lot_id = lot_tmp
-                models._logger.error('{} {} {} {}'.format(
-                    check.lot_id.name, check.component_id.display_name, check.quality_state, self.lot_id.name
+                models._logger.error('{} {} {} {} {} {}'.format(
+                    check.lot_id.name, check.component_id.display_name, check.quality_state, self.lot_id.name,
+                    self.current_quality_check_id, check.id
                 ))
                 if check.quality_state == 'none':
                     models._logger.error('action_next')
