0fcb765e6331be9af57c864b1cdb27fa1469a775
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-20 14:56:59 -0300

    debug

--- dimabe_manufacturing/models/mrp_workorder.py
+++ dimabe_manufacturing/models/mrp_workorder.py
@@ -124,7 +124,7 @@ class MrpWorkorder(models.Model):
 
         while self.current_quality_check_id:
             check = self.current_quality_check_id
-            if not self.check.component_is_byproduct:
+            if not check.component_is_byproduct:
                 check.qty_done = 0
                 self.action_skip()
             else:
