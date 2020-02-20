57037c336b18b7ac7f6ae001f688c7b8fe4ad44f
Marcelo <cheloz_20@hotmail.com>
2020-02-20 15:19:15 -0300

    otro mas

--- dimabe_purchase_process/models/purchase_order.py
+++ dimabe_purchase_process/models/purchase_order.py
@@ -51,8 +51,8 @@ class PurchaseOrder(models.Model):
         #            'boss_approval_id': self.env.user.id,
          #           'boss_approval_date': fields.datetime.now()
           #      })
-        res = super(PurchaseOrder, self).action_rfq_send()
-        return res
+        #res = super(PurchaseOrder, self).action_rfq_send()
+        #return res
       
 
     @api.multi
