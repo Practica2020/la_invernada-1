d677b8aa731123d88d1d49a24750568f361b755f
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-21 10:32:13 -0300

    debug

--- dimabe_manufacturing/models/stock_production_lot.py
+++ dimabe_manufacturing/models/stock_production_lot.py
@@ -35,8 +35,27 @@ class StockProductionLot(models.Model):
         for item in self:
             res = super(StockProductionLot, self).write(values)
             counter = 0
-            for serial in item.stock_production_lot_serial_ids:
-                counter += 1
-                tmp = '00{}'.format(counter)
-                serial.serial_number = item.name + tmp[-3:]
+            if item.is_standard_weight:
+                for counter in item.qty_standard_serial:
+                    tmp = '00{}'.format(counter)
+                    serial = item.stock_production_lot_serial_ids.filtered(
+                        lambda a: a.serial_number == item.name + tmp[-3:]
+                    )
+                    if serial:
+                        serial.update({
+                            'display_weight': item.standard_weight
+                        })
+                    else:
+                        item.env['stock.production.lot.serial'].create({
+                            'stock_production_lot_serial_id': item.id,
+                            'dislay_weight': item.standard_weight,
+                            'serial_number': item.name + tmp[-3:],
+                            'belong_to_prd_lot': True
+                        })
+            else:
+
+                for serial in item.stock_production_lot_serial_ids:
+                    counter += 1
+                    tmp = '00{}'.format(counter)
+                    serial.serial_number = item.name + tmp[-3:]
             return res
