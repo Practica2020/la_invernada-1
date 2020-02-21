95475cbd1db04a9f1b86c7e38d9d88ddab466801
Marcelo <cheloz_20@hotmail.com>
2020-02-21 12:57:07 -0300

    agregada clase transport

---
+++ dimabe_reception/models/transport.py
@@ -0,0 +1,30 @@
+from odoo import models, fields, api 
+class Transport(models.Model):
+    _name="custom_transport"
+        
+    transport_patent = fields.Char(
+        'Patente ',
+        related='carrier_id.transport_patent'
+    )
+
+    transport_is_truck = fields.Boolean(
+        'Es camión?',
+        related='carrier_id.transport_is_truck'
+    )
+
+    @api.model
+    def create(self, values_list):
+        values_list = self._prepare_data(values_list)
+        return super(Transport, self).create(values_list)
+
+    @api.multi
+    def write(self, vals):
+        vals = self._prepare_data(vals)
+        return super(Transport, self).write(vals)
+
+    def _prepare_data(self, values_list):
+        if 'transport_patent' in values_list:
+            values_list['transport_patent'] = str.upper(values_list['transport_patent'])
+        if 'transport_is_truck' in values_list:
+            values_list['transport_is_truck'] = str.upper(values_list['transport_is_truck'])
+        return values_list
