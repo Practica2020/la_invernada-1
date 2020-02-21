c1f14247707067cd382159b614ddd6fc71138407
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-21 12:24:40 -0300

    debug

--- dimabe_manufacturing/models/stock_production_lot_serial.py
+++ dimabe_manufacturing/models/stock_production_lot_serial.py
@@ -121,7 +121,7 @@ class StockProductionLotSerial(models.Model):
             )
 
             move_line = stock_move.active_move_line_ids.filtered(
-                lambda a: a.lot_id.id == item.stock_production_lot_id.id
+                lambda a: a.lot_id.id == item.stock_production_lot_id.id and a.product_qty == item.display_weight
             )
 
             stock_quant = item.stock_production_lot_id.quant_ids.filtered(
