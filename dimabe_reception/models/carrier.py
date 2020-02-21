06d5d87fb2947ab29895ad8446a3e5fa1af36c7f
Marcelo <cheloz_20@hotmail.com>
2020-02-21 10:44:59 -0300

    lolz

--- dimabe_reception/models/carrier.py
+++ dimabe_reception/models/carrier.py
@@ -17,9 +17,9 @@ class Carrier(models.Model):
 
     cell_number = fields.Char('Número de celular')
 
-    #truck_patent = fields.Char('Patente Camión')
+    truck_patent = fields.Char('Patente Camión')
     
-    #cart_patent = fields.Char('Patente Carro')
+    cart_patent = fields.Char('Patente Carro')
 
     @api.model
     def create(self, values_list):
