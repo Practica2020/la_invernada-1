3b84a16d84b84985eb1a86a9b454c69ddfd0b743
Marcelo <cheloz_20@hotmail.com>
2020-02-24 15:16:36 -0300

    chill

--- dimabe_export_order/models/custom_shipping_company.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-
-class CustomShippingCompany(models.Model):
-
-    _name = 'custom.shipping.company'
-
-    name = fields.Char(string='Naviera', required=True)
--- dimabe_quality_integration/models/stock_production_lot.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models, api
-
-
-class StockProductionLot(models.Model):
-    _inherit = 'stock.production.lot'
-
-    quality_analysis_id = fields.Many2one('quality.analysis', 'Análisis de Calidad')
-
