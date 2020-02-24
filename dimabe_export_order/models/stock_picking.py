01bf341d6c8662464837e21b42f76f9ece50b81d
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-24 14:38:53 -0300

    test -fix error

--- dimabe_export_order/models/stock_picking.py
+++ dimabe_export_order/models/stock_picking.py
@@ -203,7 +203,7 @@ class StockPicking(models.Model):
 
     @api.multi
     def generate_report(self):
-        models.logger(self.truck_in_date)
+        models._logger.error(self.truck_in_date)
         return self.env.ref('dimabe_export_order.action_dispatch_label_report') \
             .report_action(self.picture)
 
