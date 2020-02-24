925a1cafc1eefd8bcafc167ee76a6352f4ff9fc3
felipecarocadimabe <felipe.caroca@dimabe.cl>
2020-02-24 14:54:38 -0300

    debug

--- dimabe_reception/models/stock_picking.py
+++ dimabe_reception/models/stock_picking.py
@@ -162,8 +162,9 @@ class StockPicking(models.Model):
                                             self.picking_type_id.name))
 
         self.is_mp_reception = self.reception_type_selection == 'mp' or\
+                               self.picking_type_id.warehouse_id.name and\
                                'Materia Prima' in self.picking_type_id.warehouse_id.name and \
-                               'Recepciones' in self.picking_type_id.name
+                               self.picking_type_id.name and 'Recepciones' in self.picking_type_id.name
 
     @api.one
     @api.depends('production_net_weight', 'tare_weight', 'gross_weight', 'move_ids_without_package')
