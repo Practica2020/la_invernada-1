2f198e5eac0658f7fbd8f43ce06a99e23ddb3091
dimabe-odoo <desarrolloti@dimabe.cl>
2020-02-24 15:46:11 -0300

    test

--- dimabe_purchase_process/models/purchase_order.py
+++ dimabe_purchase_process/models/purchase_order.py
@@ -49,7 +49,7 @@ class PurchaseOrder(models.Model):
                     'boss_approval_id': self.env.user.id,
                     'boss_approval_date': fields.datetime.now()
                 })
-            models._logger.error(item)
+            models._logger.error(item.state)
         res = super(PurchaseOrder, self).action_rfq_send()
         return res
 
